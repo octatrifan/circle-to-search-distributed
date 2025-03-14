5ae7b30e935011f2262ee941d66d873f4af3c853
Waleed <50507337+Waleed-Helal@users.noreply.github.com>
2025-03-13 23:23:52 -0700

    Add files via upload

---
+++ src/components/SearchResults.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+function SearchResults({ results }) {
+  if (!results || results.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className="results-container">
+      <h3>Search Results</h3>
+      <div className="results-grid">
+        {results.map((imageUrl, index) => (
+          <img key={index} src={imageUrl} alt={`Result ${index + 1}`} className="result-image" />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default SearchResults;
+
+
+// the return must be like this
+// {
+//     "images": [
+//       "http://master-ip:5000/static/result1.jpg",
+//       "http://master-ip:5000/static/result2.jpg"
+//     ]
+//   }
+  
+/*
+// import React from "react";
+
+// function SearchResults({ results }) {
+//   if (!results || results.length === 0) {
+//     return null; 
+//   }
+
+//   return (
+//     <div className="results-container">
+//       <h3>Search Results</h3>
+//       <pre className="results-text">{JSON.stringify(results, null, 2)}</pre>
+//     </div>
+//   );
+// }
+
+// export default SearchResults;
+*/
