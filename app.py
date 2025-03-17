1395cf9f78624ddc6c32e9d118581cd62ea7280e
Waleed <50507337+Waleed-Helal@users.noreply.github.com>
2025-03-17 02:29:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,46 +3,35 @@ from image_search import search_similar_images
 from insert_image import insert_image_to_database
 from metadata_op import get_database_loading_status
 from flask_cors import CORS 
+import os 
+
+
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}}) 
 
-UPLOAD_FOLDER = "uploadsWaleedTest"  # Folder to temporarily store uploaded images
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route('/image_search', methods=['POST'])
 def image_search():
-    # data = request.json
-    # if 'image_path' not in data:
-    #     return jsonify({"error": "Missing image_path parameter"}), 400
+    print(request.get_json())
+    data = request.json
+    if 'image_path' not in data:
+        return jsonify({"error": "Missing image_path parameter"}), 400
 
-    # image_path = data['image_path']
-    # results = search_similar_images(image_path)
-
-    # response = {
-    #     "images": [img[0] for img in results],
-    #     "similarity_score": [img[1] for img in results]
-    # }
-    #return jsonify(response)
-    if 'image' not in request.files:
-        return jsonify({"error": "Missing image file"}), 400
-
-    image = request.files['image']
-    image_path = os.path.join(UPLOAD_FOLDER, image.filename)
-    image.save(image_path)  # Save image temporarily
-
-    # Call image search function
+    image_path = data['image_path']
     results = search_similar_images(image_path)
 
     response = {
         "images": [img[0] for img in results],
         "similarity_score": [img[1] for img in results]
     }
-
     return jsonify(response)
+
+
 
 @app.route('/insert_images', methods=['POST'])
 def insert_images():
+    print(request.get_json())
     data = request.json
     if 'image_paths' not in data:
         return jsonify({"error": "Missing image_paths parameter"}), 400
@@ -64,8 +53,9 @@ def insert_images():
             "message": "Some images could not be inserted.",
             "not_inserted": failure_list
         }
-
+    
     return jsonify(response)
+
 
 @app.route('/database_status', methods=['GET'])
 def database_status():
